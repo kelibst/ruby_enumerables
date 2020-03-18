c8db5dffa489651814dc8cf1478b9d70b545b152
kelibst <kbooster17@gmail.com>
2020-03-18 13:14:33 +0000

    made changes according to review

--- my_enum.rb
+++ my_enum.rb
@@ -1,5 +1,7 @@
 module Enumerable
   def my_each
+    return to_enum(:my_each) unless block_given?
+
     for x in 0...size # rubocop:disable Style/For
       yield self[x]
     end
@@ -7,58 +9,49 @@ module Enumerable
   end
 
   def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
     for x in 0...size # rubocop:disable Style/For
       yield(self[x], x)
       self # rubocop:disable Lint/Void
     end
   end
 
-  def my_select
+  def my_select(&block)
+    return to_enum(:my_select) unless block_given?
+
     ar = []
-
-    my_each { |e| ar.push(e) if yield e }
-
+    if block_given?
+      my_each { |e| ar.push(e) if yield e }
+    end
     ar
   end
 
   def my_all?(reg = nil)
-    res = true
-
-    my_each do |e|
-      if reg.nil?
-        res = false unless yield e
-      end
-      unless reg.nil?
-        res = false unless e =~ reg
-      end
+    if !reg.nil?
+      my_each { |x| return false unless reg == x }
+    elsif block_given?
+      my_each { |x| return false unless yield x }
+    else
+      my_each { |x| return false unless x }
     end
-    res
+    true
   end
 
   def my_any?(reg = nil)
     res = false
-    my_each do |e|
-      if reg.nil?
-        res = true if yield e
-      end
-      unless reg.nil?
-        res = true if e =~ reg
-      end
+    if !reg.nil?
+      my_each { |x| res = true if reg == x }
+    elsif block_given?
+      my_each { |x| res = true if yield(x) }
+    else
+      my_each { |x| res = true if x }
     end
     res
   end
 
-  def my_none?(reg = nil)
-    res = true
-    my_each do |e|
-      if reg.nil?
-        res = false if yield e
-
-      elsif e =~ reg
-        res = false
-      end
-    end
-    res
+  def my_none?(reg = nil, &block)
+    !my_any?(reg, &block)
   end
 
   def my_count(list = nil)
@@ -80,15 +73,18 @@ module Enumerable
     arr = self.class == Range ? to_a : self
     res = []
 
+    return to_enum(:my_map) unless block_given?
+
     if prod.nil?
       arr.my_each { |m| res.push(yield(m)) }
+
     else
       arr.my_each { |m| res.push(prod.call(m)) }
     end
     res
   end
 
-  def my_inject(cum = nil)
+  def my_inject(cum = nil, reg = nil)
     arr = self.class == Range ? to_a : self
     if cum.nil?
       cum = arr[0]
@@ -98,8 +94,17 @@ module Enumerable
     end
 
     for x in index...size # rubocop:disable Style/For
-      cum = yield(cum, arr[x])
+      if block_given?
+        cum = yield(cum, arr[x])
+      elsif cum.is_a?(Symbol)
+        cum = arr.reduce(:+)
+      elsif reg.is_a?(Symbol)
+        arr_duce = arr.reduce(:+)
+      end
     end
+
+    cum = cum += arr_duce if reg.is_a?(Symbol)
+
     cum
   end
 end
